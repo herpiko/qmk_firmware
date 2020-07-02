8b54745fc8f39bddccd95242400e660c88a6e2dc
Herpiko Dwi Aguno <herpiko@gmail.com>
2020-07-02 20:09:43 +0700

    Add mouse navigation support.

--- keyboards/handwired/unk/keymaps/default/keymap.c
+++ keyboards/handwired/unk/keymaps/default/keymap.c
@@ -13,6 +13,12 @@ enum custom_keycodes {
   COLEMAK,
 };
 
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_TIME_TO_MAX 120
+#define MOUSEKEY_WHEEL_DELAY 0
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_DELAY 0
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -27,27 +33,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_HOME,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,                  KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    KC_LBRC,    KC_RBRC,    KC_BSLASH,
     KC_END,     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,      KC_G,                           KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN,    KC_QUOT,    KC_ENT,
     KC_PSCREEN, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,                           KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH,    KC_LSFT,    KC_PGUP,
-    LOWER,      KC_LCTL, KC_LALT, KC_LGUI, RAISE,   KC_SPACE,                                         KC_SPACE,  RAISE,   KC_RALT, KC_RCTL,    KC_DELETE,  KC_PGDOWN
+    LOWER,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPACE,                                           KC_SPACE,  RAISE,   KC_RALT, KC_RCTL,    KC_DELETE,  KC_PGDOWN
   ),
   [_COLEMAK] = LAYOUT(
     KC_ESC,     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,                  KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS,    KC_EQL,     KC_BSPACE, 
     KC_HOME,    KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,     KC_G,                 KC_J,    KC_L,    KC_U,      KC_Y,    KC_SCLN, KC_LBRC,    KC_RBRC,    KC_BSLASH,
     KC_END,     KC_BSPACE, KC_A,    KC_R,    KC_S,    KC_T,     KC_D,                          KC_H,    KC_N,      KC_E,    KC_I,    KC_O,       KC_QUOT,    KC_ENT,
     KC_PSCREEN, KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                          KC_K,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH,    KC_LSFT,    KC_PGUP,
-    LOWER,      KC_LCTL,   KC_LALT, KC_LGUI, RAISE,   KC_SPACE,                                         KC_SPACE,  RAISE,   KC_RALT, KC_RCTL,    KC_DELETE,  KC_PGDOWN
+    LOWER,      KC_LCTL,   KC_LALT, KC_LGUI, LOWER,   KC_SPACE,                                         KC_SPACE,  RAISE,   KC_RALT, KC_RCTL,    KC_DELETE,  KC_PGDOWN
   ),
 
   [_LOWER] = LAYOUT(
-    _______, _______, _______, _______, _______,  _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
-    QWERTY,  _______, _______, _______, _______,  _______, _______,                    _______, _______, _______, _______, _______, _______, _______, _______,
-    COLEMAK, _______, _______, _______, _______,  _______, _______,                             _______, _______, _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______,  _______, _______,                             _______, _______, _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______,  _______,                                               _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______,  _______, _______, _______,                    _______,    _______,    _______,    _______,     _______,    _______, _______,
+    QWERTY,  _______, _______, _______, _______,  _______, _______,                    _______, _______,    KC_MS_UP,   _______,    _______,     _______,    _______, _______,
+    COLEMAK, _______, _______, _______, _______,  _______, _______,                             KC_MS_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_BTN2, _______, _______,
+    _______, KC_MS_ACCEL2, _______, _______, _______,  _______, _______,                        _______,    KC_PGUP,    _______,    KC_PGDOWN,   _______,    _______, KC__VOLUP, 
+    _______, _______, _______, _______, _______,  _______,                                                  KC_MS_BTN1, _______,    _______,     _______,    _______, KC__VOLDOWN
   ),
 
   [_RAISE] = LAYOUT(
     _______, _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,    KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______, _______, _______, KC_UP,   _______,  _______, _______,                   _______, _______, KC_UP,   _______,  _______, _______, _______, _______,
+    _______, _______, _______, KC_UP,   _______,  _______, _______,                   _______, _______, KC_UP,   _______, _______,  _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,                            _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______,
     _______, _______, _______, _______, _______,  _______, _______,                            _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______,  _______,                                              _______, _______, _______,  _______, _______, _______
