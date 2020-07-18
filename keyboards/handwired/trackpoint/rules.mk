f9adce00d4a1fae96075ea6b3b6b63320db2f8c0
Herpiko Dwi Aguno <herpiko@gmail.com>
2020-07-19 02:09:26 +0700

    Update UNK and trackpoint module firmware.

--- keyboards/handwired/trackpoint/rules.mk
+++ keyboards/handwired/trackpoint/rules.mk
@@ -26,4 +26,6 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 
 PS2_MOUSE_ENABLE = yes
-PS2_USE_USART = yes
+#PS2_USE_USART = yes
+#PS2_USE_INT = yes
+PS2_USE_BUSYWAIT = yes
